9d792e030466c915f2fe8805042009374d89093a
Lê Thiên An <161805046+annc19324@users.noreply.github.com>
2025-10-22 12:52:44 +0700

    cập nhật css cho hiện thị danh sách

--- client/src/components/RecordItem.js
+++ client/src/components/RecordItem.js
@@ -3,26 +3,38 @@ import React from 'react';
 const RecordItem = ({ record, handleDownload }) => {
     return (
         <li>
-            <div>
-                Sinh viên: {record.student_id} | Môn: {record.subject} | Điểm: {record.grade} |
-                Học kỳ: {record.semester}
+            <div className='info-part'>
+                <div>
+                    Sinh viên: {record.student_id}
+                </div>
+                <div>
+                    Môn: {record.subject}
+                </div>
+                <div>
+                    Điểm: {record.grade}
+                </div>
+                <div>
+                    Học kỳ: {record.semester}
+                </div>
             </div>
-            {record.ipfs_hash ? (
-                <>
-                    <a
-                        href={`http://localhost:8080/ipfs/${record.ipfs_hash}`}
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        Xem PDF
-                    </a>
-                    <button
-                        onClick={() => handleDownload(record.ipfs_hash, `bang_diem_${record.student_id}.pdf`)}
-                    >
-                        Tải xuống
-                    </button>
-                </>
-            ) : 'N/A'}
+            <div className='btn-part'>
+                {record.ipfs_hash ? (
+                    <>
+                        <a
+                            href={`http://localhost:8080/ipfs/${record.ipfs_hash}`}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            Xem PDF
+                        </a>
+                        <button
+                            onClick={() => handleDownload(record.ipfs_hash, `bang_diem_${record.student_id}.pdf`)}
+                        >
+                            Tải xuống
+                        </button>
+                    </>
+                ) : 'N/A'}
+            </div>
         </li>
     );
 };
